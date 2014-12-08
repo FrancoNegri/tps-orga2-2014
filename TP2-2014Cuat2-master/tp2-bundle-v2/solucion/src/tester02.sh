4d37aae96fa7ce2b844c64dd588031f1fed316d6
Franco <franconegri2004@gmail.com>
2014-12-08 20:36:04 -0300

    Agregados mas dibujos

--- TP2-2014Cuat2-master/tp2-bundle-v2/solucion/src/tester02.sh
+++ TP2-2014Cuat2-master/tp2-bundle-v2/solucion/src/tester02.sh
@@ -9,7 +9,7 @@ make OPTLVL=-O1 >> $DIR/logDeCompilacion.txt
 echo "Corriendo los tests"
 for VARIABLE1 in {1..1000}
 do
-   ./tp2 sierpinski -i asm lena.bmp 512 512 0 0 >> $DIR/Sier16Memo.txt
+   ./tp2 bandas -i c lena.bmp 512 512 0 0 >> $DIR/Sier16Memo.txt
 done
 
 echo "Limpiando Binarios"
